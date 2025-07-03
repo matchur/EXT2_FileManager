a1168833dfec088405a119e6cede87fc65a9a37c
Rafael Roseira Machado <rafaroseira27@outlook.com>
2025-07-03 17:10:53 -0300

    update utils

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -1,3 +1,8 @@
 #pragma once
 
+#include <cstdint>
+#include <cstdio>
+
 void print_time(unsigned int timestamp);
+int get_block_offset(uint32_t block, int base_offset, int block_size);
+FILE *get_file(const char *ptr);
